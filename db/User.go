3f2120f502a2184ea15ae02eabd9430ccd57a885
Mitsuhiro-Uesugi <1792887657@qq.com>
2024-08-20 06:03:45 +0000

    user Completed

--- db/User.go
+++ db/User.go
@@ -13,6 +13,7 @@ type User struct {
 	ID        int    `gorm:"primaryKey"`
 	PassWord  string `gorm:"not null"`
 	IsAdmin   bool   `gorm:"not null;default:false"`
+	IsDeleted bool   `gorm:"not null;default:false"`
 	LocationX float32
 	LocationY float32
 }
@@ -50,4 +51,69 @@ func (crud UserCRUD) GetUserByName(name string) (*User, error) {
 	// 使用 .Where 方法指定列名和查询条件
 	result := db.Where("user_name = ?", name).First(&res)
 	return &res, result.Error
+}
+
+func (crud UserCRUD) UpdateByObject(u *User) error {
+	db, err := GetDatabaseInstance()
+	if err != nil {
+		return err
+	}
+	result := db.Save(&u)
+	return result.Error
+}
+
+func (crud UserCRUD) GetUserByFuzzyName(name string) ([]User, error) {
+	db, err := GetDatabaseInstance()
+	if err != nil {
+		return nil, err
+	}
+	var res []User
+	result := db.Where("name LIKE ?", "%"+name+"%").Find(&res)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return res, result.Error
+}
+
+func (crud UserCRUD) DeleteUserbyName(name string) error {
+	result, err := crud.GetUserByName(name)
+	if err != nil {
+		return err
+	}
+	result.IsDeleted = true
+
+	err = crud.UpdateByObject(result)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (crud UserCRUD) GetAllUserOrdered() ([]User, error) {
+	db, err := GetDatabaseInstance()
+	if err != nil {
+		return nil, err
+	}
+
+	var top []User
+	var res []User
+	topUsers := db.Where("is_top = ?", true).Find(&top)
+	if topUsers.Error != nil {
+		return nil, topUsers.Error
+	}
+
+	result := db.Order("updated_at desc").Find(&res)
+	res = append(res, top...)
+	return res, result.Error
+}
+
+func (crud UserCRUD) GetAllUser() ([]User, error) {
+	db, err := GetDatabaseInstance()
+	if err != nil {
+		return nil, err
+	}
+
+	var res []User
+	result := db.Find(&res)
+	return res, result.Error
 }
